a8694dcaeb4b4bc0b8b24bd90ead00ffc56ba204
Ray-altq <83115152+Ray-altq@users.noreply.github.com>
2026-02-14 20:18:36 +0300

    Добавил абстрактный класс для шифрования

--- src/core/crypto/abstract.py
+++ src/core/crypto/abstract.py
@@ -1,1 +1,14 @@
+from abc import ABC, abstractmethod
 
+class EncryptionService(ABC):
+    """Абстрактный класс для шифрования"""
+    
+    @abstractmethod
+    def encrypt(self, data: bytes, key: bytes) -> bytes:
+        """Шифрует данные"""
+        pass
+    
+    @abstractmethod
+    def decrypt(self, ciphertext: bytes, key: bytes) -> bytes:
+        """Расшифровывает данные"""
+        pass
